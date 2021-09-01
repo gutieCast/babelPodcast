218af27c4cf04cfdfe2043bef23b41bcee852ea2
Nikolokoto <59317741+Nikolokoto@users.noreply.github.com>
2021-08-31 21:23:50 -0400

    without countdown

--- src/components/Home/components/Lab2021/index.jsx
+++ src/components/Home/components/Lab2021/index.jsx
@@ -9,36 +9,36 @@ import 'components/Home/components/Lab2021/lab2021.scss'
 
 const Lab2021 = () => {
 
-    const Completionist = () => <span>¡Empezamos!</span>;
+    // const Completionist = () => <span>¡Empezamos!</span>;
 
-    const renderer = ({ days, hours, minutes, seconds, completed }) => {
-        if (completed) {
-            // Render a completed state
-            return <Completionist />;
-        } else {
-            // Render a countdown
-            return (
-                <div className="grid-timer">
-                    <div className="unit-time">
-                        <span className="countdown-timer">{days}</span>
-                        <span className="tag-time"> días</span>
-                    </div>
-                    <div className="unit-time">
-                        <span className="countdown-timer">{hours}</span>
-                        <span className="tag-time"> horas</span>
-                    </div>
-                    <div className="unit-time">
-                        <span className="countdown-timer">{minutes}</span>
-                        <span className="tag-time"> minutos</span>
-                    </div>
-                    <div className="unit-time">
-                        <span className="countdown-timer">{seconds}</span>
-                        <span className="tag-time"> segundos</span>
-                    </div>
-                </div>
-            )
-        }
-    };
+    // const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    //     if (completed) {
+    //         // Render a completed state
+    //         return <Completionist />;
+    //     } else {
+    //         // Render a countdown
+    //         return (
+    //             <div className="grid-timer">
+    //                 <div className="unit-time">
+    //                     <span className="countdown-timer">{days}</span>
+    //                     <span className="tag-time"> días</span>
+    //                 </div>
+    //                 <div className="unit-time">
+    //                     <span className="countdown-timer">{hours}</span>
+    //                     <span className="tag-time"> horas</span>
+    //                 </div>
+    //                 <div className="unit-time">
+    //                     <span className="countdown-timer">{minutes}</span>
+    //                     <span className="tag-time"> minutos</span>
+    //                 </div>
+    //                 <div className="unit-time">
+    //                     <span className="countdown-timer">{seconds}</span>
+    //                     <span className="tag-time"> segundos</span>
+    //                 </div>
+    //             </div>
+    //         )
+    //     }
+    // };
 
     return (
         <section id="lab-2021">
@@ -48,14 +48,14 @@ const Lab2021 = () => {
 
             >
                 <p className="text">
-                    Nuestro Laboratorio Podcast 2021 empieza en:
+                    ¡Nuestro Laboratorio Podcast 2021 ya ha empezado!
                 </p>
-                <Countdown
+                {/* <Countdown
                     date="2021-08-31T19:00:00"
                     renderer={renderer}
                     zeroPadDays={1}
                     zeroPadTime={1}
-                />
+                /> */}
 
                 <div className="teachers-info">
                     <h3 className="title-section">
