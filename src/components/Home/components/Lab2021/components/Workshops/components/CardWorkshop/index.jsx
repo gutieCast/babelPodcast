6a2975d83da3aa909923dfa3a2834cce9510b64e
GutieCast <pgutierrezcdg@gmail.com>
2021-11-15 01:58:18 -0400

    fixed workshops cards style

--- src/components/Home/components/Lab2021/components/Workshops/components/CardWorkshop/index.jsx
+++ src/components/Home/components/Lab2021/components/Workshops/components/CardWorkshop/index.jsx
@@ -19,19 +19,19 @@ const CardWorkshop = ({ img, id, name, teacher, url1, url2 }) => {
                 onRequestClose={() => setModalIsOpen(false)}
                 preventScroll={false}
             >
-                <article className="uk-container modal-body">
+                <article className="modal-workshop-body">
                     <button uk-icon="icon: close; ratio: 2" className="uk-modal-close modal-close-btn" onClick={() => setModalIsOpen(false)}></button>
                     <header>
                         <h3 className="uk-card-title modal-title">{name}</h3>
                         <span className="modal-subtitle">{`por ${teacher}`}</span>
                     </header>
-                    <main>
-                        <div>
+                    <main className="modal-workshop-content">
+                        <div className="modal-workshop-video">
                             <span className="label-video-workshop">Video primera parte:</span>
                             <iframe width="560" height="315" src={url1} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
 
-                        <div>
+                        <div className="modal-workshop-video">
                             <span className="label-video-workshop">Video segunda parte:</span>
                             <iframe width="560" height="315" src={url2} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
